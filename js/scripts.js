4f81791d3c10adbd9453ff5abfc0639cc06f1b48
Allan <allanlumumba@gmail.com>
2019-12-10 12:40:12 +0300

    rearrange portfolio.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,4 @@
-$(document).ready(function(){
-  $("#designImage").click(function() {
-    $("#designImage").hide();
-    $("#designP").toggle();
-    $("#designP").show();
-  });
-  $("#designP").click(function(){
-    $("#designP").hide();
-    $("#designImage").toggle();
-    $("#designImage").show();
-  });
-});
-$(document).ready(function(){
-  $("#devImage").click(function() {
-    $("#devImage").hide();
-    $("#devP").toggle();
-    $("#devP").show();
-  });
-  $("#devP").click(function(){
-    $("#devP").hide();
-    $("#devImage").toggle();
-    $("#devImage").show();
-  });
-});
-$(document).ready(function(){
-  $("#prodImage").click(function() {
-    $("#prodImage").hide();
-    $("#prodP").toggle();
-    $("#prodP").show();
-  });
-  $("#prodP").click(function(){
-    $("#prodP").hide();
-    $("#prodImage").toggle();
-    $("#prodImage").show();
-  });
+
 
   
 
