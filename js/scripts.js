efad61ff145f870bbecdd1f174f0975cf39f01e4
Allan <allanlumumba@gmail.com>
2019-12-10 11:59:44 +0300

    adds hover effect.

--- js/scripts.js
+++ js/scripts.js
@@ -33,4 +33,34 @@ $(document).ready(function(){
     $("#prodImage").toggle();
     $("#prodImage").show();
   });
+
+  
+
+$(document).ready(function(){
+  $(".design").click(function(){
+    $(".show").toggle();
+    $(".click").toggle();
+  });
+
+    $(".development").click(function(){
+      $(".show1").toggle();
+      $(".click1").toggle();
+    });
+
+    
+
+  $(".product").click(function(){
+    $(".show2").toggle();
+    $(".click2").toggle();
+  });
+
+
+  $(".project").hover(function(){
+    $(this).animate({opacity:'1'});
+  },
+  function(){
+    $(this).animate({opacity:'0'});
+  });
+  
+
 });
